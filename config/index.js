7aea2e74f428d499b748d7073f7b3e7233d9b40b
shenfangda <964003502@qq.com>
2019-07-24 14:31:46 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8098,
+    port: 8038,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
